1d6a23b8fae6490636cbe10b1a66b17a41dad9cf
Stark, John <john.stark@lindab.com>
2022-12-03 20:23:28 +0100

    initial

---
+++ 2022/day1/solution.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+def part1():
+    with open("input.txt", "rt") as input_values:
+        calories = input_values.read().split("\n\n")
+        
+
+if __name__ == "__main__":
+    part1()
