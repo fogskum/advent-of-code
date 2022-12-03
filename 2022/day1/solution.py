112f10831d91aa1544e88759c263de8bbd533429
Stark, John <john.stark@lindab.com>
2022-12-03 20:38:00 +0100

    test

--- 2022/day1/solution.py
+++ 2022/day1/solution.py
@@ -1,9 +1,14 @@
 from pathlib import Path
 
 def part1():
-    with open("input.txt", "rt") as input_values:
-        calories = input_values.read().split("\n\n")
-        
+    with open("input.txt", "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            if line.strip():
+                print(int(line))
+        #all_elves = f.read().split('\n\n')
+        #each_elf = [[int(x) for x in elf.split("\n")] for elf in all_elves]
+        #print("" + str(sum(elf) for elf in each_elf))
 
 if __name__ == "__main__":
     part1()
