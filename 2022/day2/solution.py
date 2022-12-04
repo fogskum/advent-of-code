844fd0a40ef535c91a7b0252e9217697be37c6ec
Stark, John <john.stark@lindab.com>
2022-12-04 14:20:37 +0100

    d2p1 complete

--- 2022/day2/solution.py
+++ 2022/day2/solution.py
@@ -1,15 +1,10 @@
 from pathlib import Path
-from enum import Enum
+from enum import Enum, IntEnum
 
-class Shape(Enum):
+class Shape(IntEnum):
     Rock = 1
     Paper = 2
     Scissor = 3
-
-class Score(Enum):
-    Win = 6,
-    Loss = 0,
-    Draw = 3
 
 opponent_map = {
     "A": Shape.Rock,
@@ -23,18 +18,42 @@ my_map = {
     "Z": Shape.Scissor
 }
 
+def calc_score(opponent_shape, my_shape) -> int:
+    win = 6
+    draw = 3
+    score = 0
+    shape_score = int(my_shape)
+
+    if opponent_shape == my_shape:
+        # draw
+        score = (shape_score + draw)
+    elif opponent_shape == Shape.Rock:
+        if my_shape == Shape.Scissor:
+            score = shape_score
+        elif my_shape == Shape.Paper:
+            score = (shape_score + win)
+    elif opponent_shape == Shape.Paper:
+        if my_shape == Shape.Rock:
+            score = shape_score
+        elif my_shape == Shape.Scissor:
+            score = (shape_score + win)
+    elif opponent_shape == Shape.Scissor:
+        if my_shape == Shape.Rock:
+            score = (shape_score + win)
+        elif my_shape == Shape.Paper:
+            score = shape_score
+    return score
+
+# correct score = 10816
 def part1(input_values):
-    opponent_score = 0
     my_score = 0
     for round in input_values:
         opponent_shape = opponent_map[round.split(" ")[0]]
         my_shape = my_map[round.split(" ")[1]]
         print("Opponent plays " + str(opponent_shape) + ", I play " + str(my_shape))
-
-        if opponent_shape == my_shape:
-            opponent_score += (my_shape + Score.Draw)
-        elif opponent_shape == Shape.Rock and my_shape == Shape.Scissor:
-            opponent_score += 
+        my_score += calc_score(opponent_shape, my_shape)
+        
+    print( "My total score: " + str(my_score) )
 
 def get_input() -> list[str]:
     with open("input.txt", "r") as f:
