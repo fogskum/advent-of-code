87fde365853690b1a456828229ee4543423539e1
Stark, John <john.stark@lindab.com>
2022-12-04 08:13:22 +0100

    day1 complete

--- 2022/day1/solution.py
+++ 2022/day1/solution.py
@@ -1,14 +1,24 @@
 from pathlib import Path
 
-def part1():
+
+def get_sum_calories() -> list[int]:
     with open("input.txt", "r") as f:
-        lines = f.readlines()
-        for line in lines:
-            if line.strip():
-                print(int(line))
-        #all_elves = f.read().split('\n\n')
-        #each_elf = [[int(x) for x in elf.split("\n")] for elf in all_elves]
-        #print("" + str(sum(elf) for elf in each_elf))
+        # read calories and split by two line breaks
+        elves = f.read().split("\n\n")
+        # create matrix of each elvs calories
+        each_elf = [[int(item) for item in elf.split("\n")] for elf in elves]
+        # create new list of each elf's total calories
+        sum_list = [sum(elf) for elf in each_elf]
+        return sum_list
+
 
 if __name__ == "__main__":
-    part1()
+    sum_calories = get_sum_calories()
+    # part 1: get max value
+    max_calories = max(sum_calories)
+    print(max_calories)
+
+    # part 2: get sum of top 3
+    sorted_calories = sorted(sum_calories)
+    top_list = [sorted_calories[-1], sorted_calories[-2], sorted_calories[-3]]
+    print(sum(top_list))
