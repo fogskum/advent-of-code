390864962b5f781ce5b874b17219c6d952171aa3
Stark, John <john.stark@lindab.com>
2022-12-05 09:46:21 +0100

    part1 completed

--- 2022/day3/solution.py
+++ 2022/day3/solution.py
@@ -1,22 +1,30 @@
 from pathlib import Path
+from itertools import chain
 
 def part1(input_values):
     # create dictionaries of priorities
-    lowercase_prios = [{chr(i+96):i} for i in range(1,27)]
-    uppercase_prios = [{chr(i+38):i} for i in range(27,53)]
-    prio_list = lowercase_prios + uppercase_prios
+    lowercase_prios = {chr(i+96):i for i in range(1,27)}
+    uppercase_prios = {chr(i+38):i for i in range(27,53)}
+    
+    # concatenate dictionaries
+    prio_list = dict(chain.from_iterable(d.items() for d in (lowercase_prios, uppercase_prios)))
 
     # get items for each compartment    
     pairs = get_item_pairs(input_values)
     
     # look for items that appears in both compartments
+    total_score = 0
     for pair in pairs:
-        item1 = pair[0]
-        item2 = pair[1]
+        # remove duplicated chars
+        item1 = "".join(dict.fromkeys(pair[0]))
+        item2 = "".join(dict.fromkeys(pair[1]))
         for char in item1:
             if char in item2:
                 # lookup prio for current item
-                prio = prio_list[char]
+                score = prio_list[char]
+                total_score += score
+    
+    print(total_score)
 
 # create a list of items that goes in to each compartment
 def get_item_pairs(input_values):
@@ -28,9 +36,9 @@ def get_item_pairs(input_values):
         items.append([item1, item2])
     return items
 
-def get_input():
-    with open("input.txt", "r") as f:
+def get_input(filename):
+    with open(filename, "r") as f:
         return f.read().split("\n")
 
 if __name__ == "__main__":
-    part1(get_input())
+    part1(get_input("input.txt"))
