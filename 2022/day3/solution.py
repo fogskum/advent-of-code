9fcf41fc95f1e8c4e016ccc457b371bd438ee8f1
Stark, John <john.stark@lindab.com>
2022-12-05 12:16:01 +0100

    day 3 completed

--- 2022/day3/solution.py
+++ 2022/day3/solution.py
@@ -1,14 +1,30 @@
 from pathlib import Path
 from itertools import chain
 
-def part1(input_values):
-    # create dictionaries of priorities
-    lowercase_prios = {chr(i+96):i for i in range(1,27)}
-    uppercase_prios = {chr(i+38):i for i in range(27,53)}
-    
-    # concatenate dictionaries
-    prio_list = dict(chain.from_iterable(d.items() for d in (lowercase_prios, uppercase_prios)))
+# create dictionaries of priority scores based on ASCII table
+lowercase_prios = {chr(i+96):i for i in range(1,27)}
+uppercase_prios = {chr(i+38):i for i in range(27,53)}
 
+# concatenate dictionaries
+prio_dict = dict(chain.from_iterable(d.items() for d in (lowercase_prios, uppercase_prios)))
+
+# correct=2697
+def part2(input_values):
+    count = len(input_values)
+    page_size = 3
+    pages = int(count / 3)
+    score = 0
+
+    for page in range(0, pages):
+        start = (page) * page_size
+        end = start + page_size
+        group = input_values[start:end]
+        score += calc_score(group)
+
+    return score
+
+# correct answer=7737
+def part1(input_values):
     # get items for each compartment    
     pairs = get_item_pairs(input_values)
     
@@ -21,10 +37,29 @@ def part1(input_values):
         for char in item1:
             if char in item2:
                 # lookup prio for current item
-                score = prio_list[char]
+                score = prio_dict[char]
                 total_score += score
     
-    print(total_score)
+    return total_score
+
+def calc_score(group):
+    score = 0
+
+    # remove duplicated chars
+    base_item = "".join(dict.fromkeys(group[0]))
+    other_items = group[1:]
+    for char in base_item:
+        # keep track of number of hits
+        hit_count = 0
+        for other_item in other_items:
+            local_item = "".join(dict.fromkeys(other_item))
+            if char in local_item:
+                # lookup prio for current item
+                hit_count += 1
+                if hit_count == 2:
+                    score += prio_dict[char]
+    
+    return score
 
 # create a list of items that goes in to each compartment
 def get_item_pairs(input_values):
@@ -41,4 +76,10 @@ def get_input(filename):
         return f.read().split("\n")
 
 if __name__ == "__main__":
-    part1(get_input("input.txt"))
+    input_values = get_input("input.txt")
+    
+    part1_result = part1(input_values)
+    print(int(part1_result))
+
+    part2_result = part2(input_values)
+    print(int(part2_result))
