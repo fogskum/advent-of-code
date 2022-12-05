5f3acf91f4826a7840dbae01e1a54b4e2b539208
Stark, John <john.stark@lindab.com>
2022-12-05 15:50:39 +0100

    tbc

--- 2022/day5/solution.py
+++ 2022/day5/solution.py
@@ -1,11 +1,26 @@
 from pathlib import Path
 
-def part1():
-    return
+class Instruction:
+    Quantity = 0
+    FromCrate = 0
+    ToCrate = 0
+
+def get_stack_count(input_values):
+    line_num = 0
+    for line in input_values:
+        if line == "\n":
+            break
+        line_num += 1
+
+    print(line_num)
+    numbers = input_values[line_num-1].strip().split("   ")
+    print(numbers)
 
 def get_input(filename):
     with open(filename, "r") as f:
-        return
+        return f.readlines()
 
 if __name__ == "__main__":
-    part1()
+    input_values = get_input("sample_input.txt")
+    get_stack_count(input_values)
+    
