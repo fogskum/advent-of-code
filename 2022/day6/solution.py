4f75e229749177b5111039a83ff29e470a1167cc
Stark, John <john.stark@lindab.com>
2022-12-06 15:48:07 +0100

    minor change

--- 2022/day6/solution.py
+++ 2022/day6/solution.py
@@ -1,8 +1,7 @@
 from pathlib import Path
 from collections import Counter
 
-def get_start_pos(input_values):
-    window_size = 4
+def get_start_pos(input_values, window_size):
     start_idx = 0
     while True:
         end_idx = start_idx + window_size
@@ -20,14 +19,21 @@ def is_marker(seq):
     return True
 
 def part1(input_values):
-    return get_start_pos(input_values)
+    return get_start_pos(input_values, 4)
+
+def part2(input_values):
+    return get_start_pos(input_values, 14) - 1
 
 def get_input(filename):
     with open(filename, "r") as f:
-        return f.read()
+        return f.readline()
 
 if __name__ == "__main__":
     input_values = get_input("input.txt")
     part1_result = part1(input_values)
     part1_expected = 1909
     assert part1_result == part1_expected
+
+    part2_result = part2(input_values)
+    part2_expected = 3380
+    assert part2_result == part2_expected
