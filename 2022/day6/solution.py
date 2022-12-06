bf23b0ffe21f9bb646731ac9cb41ee0282c0b0dc
Stark, John <john.stark@lindab.com>
2022-12-06 14:09:34 +0100

    initial commit

---
+++ 2022/day6/solution.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+
+
+def get_input(filename):
+    with open(filename, "r") as f:
+        return f.read()
+
+if __name__ == "__main__":
+    input_values = get_input("sample_input.txt")
+    print(input_values)
