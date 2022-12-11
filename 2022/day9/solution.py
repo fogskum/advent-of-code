81b586789bdb404f7e6f16303f4fd6523ea104ae
Stark, John <john.stark@lindab.com>
2022-12-11 05:53:04 +0100

    some code at least

--- 2022/day9/solution.py
+++ 2022/day9/solution.py
@@ -1,11 +1,39 @@
 from pathlib import Path
 
+directions = {
+    "R":(1,0),
+    "L":(-1,0),
+    "U":(0,-1),
+    "D":(0,1)
+}
+
 def part2():
     return 0
 
-def part1():
+def is_tail_close(pos):
+    
+    return False
+
+def part1(lines):
+    hx = hy = 0
+    tx = ty = 0
+    positions = []
+    for line in lines:
+        direction = directions[line.split()[0]]
+        length = int(line.split()[1])
+        for step in range(1, length):
+            hx += direction[0]
+            hy += direction[1]
+            
     return 0
 
+def get_input(filename):
+    with open(filename, "r") as f:
+        return f.read().split("\n")
+
 if __name__ == "__main__":
-    part1_result = part1()
+    input_values = get_input("sample_input.txt")
+    print(input_values)
+    
+    part1_result = part1(input_values)
     print(part1_result)
