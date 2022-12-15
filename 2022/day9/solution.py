bdc32e9effadb4aaea9c049bbbe21b1caa282a43
Stark, John <john.stark@lindab.com>
2022-12-15 15:18:44 +0100

    test

--- 2022/day9/solution.py
+++ 2022/day9/solution.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import sys
 
 directions = {
     "R":(1,0),
@@ -7,75 +8,72 @@ directions = {
     "D":(0,1)
 }
 
-class Point:
-    def __init__(self, x, y) -> None:
+class Knot:
+    def __init__(self, x, y, symbol) -> None:
         self.x = x
         self.y = y
+        self.symbol = symbol
 
-# def track_tail(lines, rope):
-#     # count initial tail position
-#     head_pos = rope[0]
-#     tail_pos = rope[-1]
-#     tail_positions = {(tail_pos.x, tail_pos.y):True}
+def part2(lines):
+    rope = [Knot(0, 0, str(i)) for i in range(10)]
 
-#     for line in lines:
-#         direction = line.split()[0]
-#         direction_vector = directions[direction]
-#         length = int(line.split()[1])
-#         for step in range(1, length+1):
-#             # save previous position
-#             prev_pos = Point(head_pos.x, head_pos.y)
-#             head_pos.x += direction_vector[0]
-#             head_pos.y += direction_vector[1]
-#             # head has a new pos, check distance to next knot
-#             diff_x = abs(head_pos.x - tail_pos.x)
-#             diff_y = abs(head_pos.y - tail_pos.y)
-
-#             if diff_x > 1 or diff_y > 1:
-#                 tail_pos.x = prev_pos.x
-#                 tail_pos.y = prev_pos.y
-#                 tail_positions[(tail_pos.x, tail_pos.y)] = True
-    
-#     return len(tail_positions)
-
-def track_tail(lines, rope):
     # count initial tail position
     head_pos = rope[0]
-    tail_pos = rope[-1]
-    tail_positions = {(tail_pos.x, tail_pos.y):True}
+    tail = rope[-1]
+    tail_positions = {(tail.x, tail.y):True}
 
     for line in lines:
         direction = line.split()[0]
         direction_vector = directions[direction]
         steps = int(line.split()[1])
         rope_len = len(rope)
-        for _ in range(1, steps+1):
-            for i in range(0, rope_len):
-                prev_knot = rope[i]
-                knot = rope[i+1]
+        for _ in range(steps):
+            prev_pos = Knot(head_pos.x, head_pos.y, head_pos.symbol)
+            head_pos.x += direction_vector[0]
+            head_pos.y += direction_vector[1]
+            next_knot = rope[1]
+            diff_x = abs(head_pos.x - next_knot.x)
+            diff_y = abs(head_pos.y - next_knot.y)            
+            if diff_x > 1 or diff_y > 1:
+                # move all other knots
+                for i in range(1, rope_len):
+                    prev_tail_pos = Knot(tail.x, tail.y, tail.symbol)
+                    temp_pos = Knot(rope[i].x, rope[i].y, rope[i].symbol)
+                    rope[i].x = prev_pos.x
+                    rope[i].y = prev_pos.y
+                    # did the tail get a new position?
+                    if tail.x != prev_tail_pos.x or tail.y != prev_tail_pos.y:
+                        tail_positions[(tail.x, tail.y)] = True
+                    prev_pos = Knot(temp_pos.x, temp_pos.y, temp_pos.symbol)
 
-                if i == 0:
-                    head_pos.x += direction_vector[0]
-                    head_pos.y += direction_vector[1]
-                else:
-                    diff_x = abs(knot.x - prev_knot.x)
-                    diff_y = abs(knot.y - prev_knot.y)
-                    if diff_x > 1 or diff_y > 1:
-                        knot.x = prev_knot.x
-                        knot.y = prev_knot.y
-                        if i == rope_len-1:
-                            tail_positions[(knot.x, knot.y)] = True
-                    prev_knot = knot
-    
     return len(tail_positions)
 
 def part1(input_values):
-    rope = [Point(0, 0) for _ in range(2)]
-    return track_tail(input_values, rope)
+    rope = [Knot(0, 0, str(i)) for i in range(2)]
+    # count initial tail position
+    head_pos = rope[0]
+    tail_pos = rope[-1]
+    tail_positions = {(tail_pos.x, tail_pos.y):True}
 
-def part2(input_values):
-    rope = [Point(0, 0) for _ in range(10)]
-    return track_tail(input_values, rope)
+    for line in input_values:
+        direction = line.split()[0]
+        direction_vector = directions[direction]
+        length = int(line.split()[1])
+        for step in range(1, length+1):
+            # save previous position
+            prev_pos = Knot(head_pos.x, head_pos.y, head_pos.symbol)
+            head_pos.x += direction_vector[0]
+            head_pos.y += direction_vector[1]
+            # head has a new pos, check distance to next knot
+            diff_x = abs(head_pos.x - tail_pos.x)
+            diff_y = abs(head_pos.y - tail_pos.y)
+
+            if diff_x > 1 or diff_y > 1:
+                tail_pos.x = prev_pos.x
+                tail_pos.y = prev_pos.y
+                tail_positions[(tail_pos.x, tail_pos.y)] = True
+    
+    return len(tail_positions)
 
 def get_input(filename):
     with open(filename, "r") as f:
