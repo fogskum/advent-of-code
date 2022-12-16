3794e3d97450623881e485787888edce93301ba3
Stark, John <john.stark@lindab.com>
2022-12-16 15:25:28 +0100

    test

--- 2022/day9/solution.py
+++ 2022/day9/solution.py
@@ -1,12 +1,18 @@
 from pathlib import Path
 import sys
+import curses
 
 directions = {
     "R":(1,0),
     "L":(-1,0),
-    "U":(0,-1),
-    "D":(0,1)
+    "U":(0,1),
+    "D":(0,-1)
 }
+
+start_x = 0
+start_y = 0
+
+stdscr = curses.initscr()
 
 class Knot:
     def __init__(self, x, y, symbol) -> None:
@@ -14,11 +20,27 @@ class Knot:
         self.y = y
         self.symbol = symbol
 
+    def SetPos(self, x, y):
+        self.x = x
+        self.y = y
+
+    def IncPos(self, x, y):
+        self.x += x
+        self.y += y
+
+def draw(rope):
+    for knot in rope:
+        stdscr.addstr(knot.x, knot.y, knot.symbol)
+    stdscr.clear()
+    stdscr.refresh()
+
 def part2(lines):
-    rope = [Knot(0, 0, str(i)) for i in range(10)]
+    # start_x = int(curses.COLS / 2)
+    # start_y = int(curses.LINES / 2)
+    rope = [Knot(start_x, start_y, str(i)) for i in range(10)]
 
     # count initial tail position
-    head_pos = rope[0]
+    head = rope[0]
     tail = rope[-1]
     tail_positions = {(tail.x, tail.y):True}
 
@@ -26,25 +48,26 @@ def part2(lines):
         direction = line.split()[0]
         direction_vector = directions[direction]
         steps = int(line.split()[1])
-        rope_len = len(rope)
+        knot_count = len(rope)
         for _ in range(steps):
-            prev_pos = Knot(head_pos.x, head_pos.y, head_pos.symbol)
-            head_pos.x += direction_vector[0]
-            head_pos.y += direction_vector[1]
-            next_knot = rope[1]
-            diff_x = abs(head_pos.x - next_knot.x)
-            diff_y = abs(head_pos.y - next_knot.y)            
-            if diff_x > 1 or diff_y > 1:
-                # move all other knots
-                for i in range(1, rope_len):
-                    prev_tail_pos = Knot(tail.x, tail.y, tail.symbol)
-                    temp_pos = Knot(rope[i].x, rope[i].y, rope[i].symbol)
-                    rope[i].x = prev_pos.x
-                    rope[i].y = prev_pos.y
-                    # did the tail get a new position?
-                    if tail.x != prev_tail_pos.x or tail.y != prev_tail_pos.y:
-                        tail_positions[(tail.x, tail.y)] = True
-                    prev_pos = Knot(temp_pos.x, temp_pos.y, temp_pos.symbol)
+            head.IncPos(direction_vector[0], direction_vector[1])
+            for i in range(1, knot_count):
+                diff_x = rope[i-1].x - rope[i].x
+                diff_y = rope[i-1].y - rope[i].y
+                if diff_x == 0 or diff_y == 0:
+                    continue
+
+                if diff_x > 1:
+                    rope[i].IncPos(1, 0) # move right
+                if diff_x < 1:
+                    rope[i].IncPos(-1, 0) # move left
+
+                if diff_y > 1 and abs(diff_x) == 1:
+                    rope[i].IncPos(1, 1) # move up
+                if diff_y < 1:
+                    rope[i].IncPos(0, -1) # move down
+
+                tail_positions[(tail.x, tail.y)] = True
 
     return len(tail_positions)
 
@@ -83,7 +106,7 @@ if __name__ == "__main__":
     input_values = get_input("sample_input.txt")
     
     part1_result = part1(input_values)
-    print(part1_result)
+    #print(part1_result)
     #part1_expected = 5779
     #assert part1_result == part1_expected
 
